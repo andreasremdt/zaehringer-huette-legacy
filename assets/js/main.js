477fdeed450c241368d52bab9f4566730d529aa0
Andreas Remdt <andreas.remdt@gmail.com>
2018-09-30 17:56:37 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+function enableActiveCard(month) {
+  if (document.querySelectorAll('[data-card]').length > 0) {
+    if (month > 6 && month < 9) {
+      document.querySelector('[data-card="summer"]').classList.add('has-ribbon');
+    } else {
+      document.querySelector('[data-card="winter"]').classList.add('has-ribbon');
+    }
+  }
+}
+
+function scrollIntoView(event) {
+  event.preventDefault();
+  
+  var target = document.querySelector(this.hash);
+
+  if (target) {
+    target.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth'
+    });
+  }
+}
+
+function toggleMobileMenu(menu, button) {
+  var icon = button.querySelector('use');
+  var text = button.querySelector('span');
+
+  if (!menu.classList.contains('is-open')) {
+    menu.classList.add('is-open');
+    text.textContent = 'Menu schließen';
+    icon.setAttribute('xlink:href', icon.getAttribute('xlink:href').replace('#icon-menu', '#icon-close'));
+  } else {
+    menu.classList.remove('is-open');
+    text.textContent = 'Menu anzeigen';
+    icon.setAttribute('xlink:href', icon.getAttribute('xlink:href').replace('#icon-close', '#icon-menu'));
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Active Pricing Card
+  var month = new Date().getMonth();
+
+  enableActiveCard(month);
+
+
+
+  // Smooth Scrolling
+  var links = document.querySelectorAll('[data-smooth-scroll]');
+  
+  [].forEach.call(links, function(link) {
+    link.addEventListener('click', scrollIntoView);
+  });
+  
+
+
+  // Responsive Menu
+  var menuToggle = document.querySelector('[data-menu-button]');
+  var menuWrapper = document.querySelector('[data-menu]');
+
+  menuToggle.addEventListener('click', function() {
+    toggleMobileMenu(menuWrapper, menuToggle);
+  });
+
+
+
+  // Litebox
+  new Litebox();
+});
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function() {
+  return gulp.src('assets/scss/styles.scss')
+    .pipe(sass({ outputStyle: 'nested' }).on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('assets/scss/**/*.scss', ['css']);
+});
