4c9eda0cdb9bb0f064e969ea5de90362f4456d68
Andreas Remdt <andreas.remdt@gmail.com>
2018-10-06 11:55:38 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main class="section-white text-center">
+  <div class="container">
+    <h1 class="page-title">404 - Seite nicht gefunden</h1>
+    <p>Leider konnte die von Ihnen gewünschte Seite nicht gefunden werden.<br>Vergewissern Sie sich, dass Sie die Adresse korrekt geschrieben haben.</p>
+    <a href="<?php bloginfo('url'); ?>" class="button">Zur Startseite</a>
+  </div>
+</main>
+
+<?php get_footer(); ?>
