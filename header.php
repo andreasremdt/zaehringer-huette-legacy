d7b786611cf37b89ddbc413829902c0823068664
Andreas Remdt <andreas.remdt@gmail.com>
2018-10-06 12:04:44 +0200

    fix error in php lang attr in header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>">
+<html <?php language_attributes(); ?> dir="ltr">
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
